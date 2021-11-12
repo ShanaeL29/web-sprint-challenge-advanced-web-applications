985836891e9003b9d30eef8e611076ac7a680236
Shanae <sleslie18@gmail.com>
2021-11-12 10:38:31 -0500

    Added imports to Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import styled from "styled-components";
+import { useHistory } from "react-router-dom";
 
 const initialErrors = {
   credentials: {
